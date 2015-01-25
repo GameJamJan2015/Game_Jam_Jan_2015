8fa2ab4fc971ddb6718deb856560e88d55411d10
Simon Bothén <caresilabs@gmail.com>
2015-01-25 05:30:34 +0100

    More on shadow

--- Project/core/src/wdwdn/entity/Shadow.java
+++ Project/core/src/wdwdn/entity/Shadow.java
@@ -1,6 +1,10 @@
 package wdwdn.entity;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.actions.AddListenerAction;
 
+import com.badlogic.gdx.utils.Array;
+import wdwdn.Assets;
 import wdwdn.World;
 
 
@@ -9,7 +13,14 @@ public class Shadow extends Enemy {
 	public Shadow(World world, float x, float y,
 			float radius) {
 		super(world, x, y, .7f, 1, radius);
-		
+
+		Array<TextureRegion> walk = new Array<TextureRegion>();
+		for (int i = 0; i < 22; i++) {
+			walk.add(new TextureRegion(Assets.shadow, i * 64, 0, 64, 64));
+		}
+		addAnimation("walk", new Animation(.1f, walk));
+		setAnimation("walk");
+
 		speed = 0.5f;
 	}
 	public void update(float delta) {
